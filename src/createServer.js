9aadb3d55c343e888a6c99bc97c850311cba1d03
Stanislav Snizhkovyi <stas.snezhkovoy@gmail.com>
2025-09-03 20:02:30 +0300

    fix 2

--- src/createServer.js
+++ src/createServer.js
@@ -1,19 +1,17 @@
 const http = require('http');
-
-const { convertToCase } = require('./convertToCase/convertToCase.js');
-
-const SUPPORTED_CASES = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+const { convertToCase } = require('./convertToCase');
 
 function createServer() {
   const server = http.createServer((req, res) => {
-    const normalized = new URL(req.url, `http://${req.headers.host}`);
+    const url = new URL(req.url, `http://${req.headers.host}`);
 
-    const textToConvert = normalized.pathname.slice(1);
-    const toCase = normalized.searchParams.get('toCase');
+    const toCase = url.searchParams.get('toCase');
+    const text = url.pathname.slice(1);
 
+    const correctCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
     const errors = [];
 
-    if (!textToConvert) {
+    if (!text) {
       errors.push({
         message: `Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
       });
@@ -23,34 +21,37 @@ function createServer() {
       errors.push({
         message: `"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
       });
-    }
-
-    if (toCase && !SUPPORTED_CASES.includes(toCase)) {
+    } else if (!correctCases.includes(toCase)) {
       errors.push({
         message: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
       });
     }
+
     res.setHeader('Content-Type', 'application/json');
 
     if (errors.length > 0) {
       res.statusCode = 400;
       res.statusMessage = 'Bad request';
-      res.end(JSON.stringify({ errors }));
+
+      const errorPayload = { errors: errors };
+
+      res.end(JSON.stringify(errorPayload));
 
       return;
     }
 
-    const result = convertToCase(textToConvert, toCase);
+    const { originalCase, convertedText } = convertToCase(text, toCase);
 
-    const fullResponse = {
+    const responsePayload = {
+      originalCase: originalCase,
       targetCase: toCase,
-      originalText: textToConvert,
-      ...result,
+      originalText: text,
+      convertedText: convertedText,
     };
 
     res.statusCode = 200;
     res.statusMessage = 'OK';
-    res.end(JSON.stringify(fullResponse));
+    res.end(JSON.stringify(responsePayload));
   });
 
   return server;
